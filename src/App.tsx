46da0c578007ff91f42a927c49c26e806fd4eed1
vitor2233 <vitorguilherme2233@gmail.com>
2024-03-04 18:50:16 -0300

    Add styled components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./Components/Button";
+
+export function App() {
+
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button />
+    </>
+  )
+}
